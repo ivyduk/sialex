02ec55b5170b46bccb68bef281e4406f133dd7d0
Ivan <iaduquea@unal.edu.co>
2022-12-28 21:20:16 -0500

    ajustado form para cargar ofertas por período

--- djsialex/administracion/views/curso.py
+++ djsialex/administracion/views/curso.py
@@ -17,13 +17,18 @@ class CursoCreate(LoginRequiredMixin, CreateView):
     form_class = CursoModelForm
     success_url = None
 
+    def get_form_kwargs(self):
+        kwargs = super(CursoCreate, self).get_form_kwargs()
+        kwargs['periodo_contextualizado_id'] = self.request.session["periodo_contextualizado_id"]
+        return kwargs
+
     def get_context_data(self, **kwargs):
         data = super(CursoCreate, self).get_context_data(**kwargs)
         if self.request.POST:
             data['cursoForm'] = data['form']
             data['formset'] = HorarioCursoFormset(self.request.POST)
         else:
-            data['cursoForm'] = self.form_class
+            data['cursoForm'] = data['form']
             data['formset'] = HorarioCursoFormset()
         return data
 
@@ -54,6 +59,7 @@ class CursoCreate(LoginRequiredMixin, CreateView):
             except IntegrityError:
                 messages.warning(self.request, 'Hubo un error al guardar el curso, revise que no exista el curso y que los campos estén correctamente diligenciados')
                 return redirect(reverse('curso_create'))
+
     def get_success_url(self):
         return reverse_lazy('curso-detail', kwargs={'pk': self.object.id})
 
